ab76700a71c9c498337dfa3cf024eb1ae9dc2086
Igor Koshelev <igor@cleverflow.ru>
2016-08-09 15:04:40 +0300

    Добавил FieldMonitor.

---
+++ Bwl.Tools/Tools/FieldMonitor.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Reflection
+
+Public Class FieldMonitor
+    Implements IDisposable
+    Public Event FieldChanged(target As Object, field As PropertyInfo)
+    Public Event FieldChangedDetailed(target As Object, field As PropertyInfo, fieldName As String, lastValue As Object, currentValue As Object)
+
+    Private _items As New List(Of FieldMonitorItem)
+    Private _thread As Threading.Thread
+    Private _monitorDelayMs As Integer
+
+    Public Class FieldMonitorItem
+        Public Property LastFieldValue As Object
+        Public Property Target As Object
+        Public Property PropertyInfo As PropertyInfo
+    End Class
+
+    Public Sub New(target As Object, Optional monitorDelayMs As Integer = 500, Optional filterByName As String = "Setting")
+        _monitorDelayMs = monitorDelayMs
+        For Each prop In target.GetType.GetProperties
+            If filterByName = "" OrElse prop.Name.Contains(filterByName) Then
+                Select Case prop.PropertyType
+                    Case GetType(String), GetType(Integer), GetType(Double), GetType(Single), GetType(Boolean)
+                        Dim val = prop.GetValue(target, Nothing)
+                        Dim info As New FieldMonitorItem
+                        info.LastFieldValue = val
+                        info.PropertyInfo = prop
+                        info.Target = target
+                        _items.Add(info)
+                End Select
+            End If
+        Next
+
+        If _items.Count > 0 Then
+            _thread = New Threading.Thread(AddressOf MonitorThread)
+            _thread.Name = "FieldMonitor Worker"
+            _thread.IsBackground = True
+            _thread.Start()
+        End If
+    End Sub
+
+    Public ReadOnly Property MonitoredFields As FieldMonitorItem()
+        Get
+            Return _items.ToArray
+        End Get
+    End Property
+
+    Private Sub MonitorThread()
+        Do
+            Try
+                For Each item In _items
+                    Try
+
+                        Dim fieldVal = item.PropertyInfo.GetValue(item.Target, Nothing)
+                        If fieldVal <> item.LastFieldValue Then
+                            RaiseEvent FieldChanged(item.Target, item.PropertyInfo)
+                            RaiseEvent FieldChangedDetailed(item.Target, item.PropertyInfo, item.PropertyInfo.Name, item.LastFieldValue, fieldVal)
+                        End If
+                        item.LastFieldValue = fieldVal
+                    Catch ex As Exception
+                    End Try
+                Next
+            Catch ex As Exception
+            End Try
+            Threading.Thread.Sleep(_monitorDelayMs)
+        Loop
+    End Sub
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' Для определения избыточных вызовов
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                Try
+                    _thread.Abort()
+                Catch ex As Exception
+                End Try
+
+                Try
+                    _items.Clear()
+                Catch ex As Exception
+                End Try
+                _items = Nothing
+                _thread = Nothing
+            End If
+        End If
+        disposedValue = True
+    End Sub
+
+    ' Этот код добавлен редактором Visual Basic для правильной реализации шаблона высвобождаемого класса.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Не изменяйте этот код. Разместите код очистки выше в методе Dispose(disposing As Boolean).
+        Dispose(True)
+        ' TODO: раскомментировать следующую строку, если Finalize() переопределен выше.
+        ' GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+End Class
+
---
+++ Bwl.Tools/ToolsTests/FieldMonitorTest.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Reflection
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class FieldMonitorTest
+    Public Class TestClass
+        Public Property Test1 As String = "1234"
+        Public Property Test2 As String
+        Public Property Test3 As Integer
+        Public Property Test4 As Single = 0.1
+        Public Property Test5 As Double
+        Public Property Test6 As Boolean = True
+    End Class
+
+    <TestMethod()>
+    Public Sub TestFieldMonitorTest()
+        Dim mark1 As Integer
+        Dim mark2 As Integer
+        Dim mark3 As Integer
+        Dim mark4 As Integer
+        Dim mark5 As Integer
+        Dim mark6 As Integer
+        Dim testclass As New TestClass
+        Dim monitor As New FieldMonitor(testclass, "Test")
+        AddHandler monitor.FieldChangedDetailed, Sub(target As Object, field As PropertyInfo, fieldName As String, lastValue As Object, currentValue As Object)
+                                                     If fieldName = "Test1" Then mark1 += 1
+                                                     If fieldName = "Test2" Then mark2 += 1
+                                                     If fieldName = "Test3" Then mark3 += 1
+                                                     If fieldName = "Test4" Then mark4 += 1
+                                                     If fieldName = "Test5" Then mark5 += 1
+                                                     If fieldName = "Test6" Then mark6 += 1 = True
+                                                 End Sub
+        testclass.Test1 = "34346436"
+        testclass.Test2 = "443563"
+        testclass.Test3 = -125
+        testclass.Test4 = 0.6
+        testclass.Test5 = -0.3424
+        testclass.Test6 = False
+        Threading.Thread.Sleep(1000)
+        Assert.AreEqual(1, mark1)
+        Assert.AreEqual(1, mark2)
+        Assert.AreEqual(1, mark3)
+        Assert.AreEqual(1, mark4)
+        Assert.AreEqual(1, mark5)
+        Assert.AreEqual(1, mark6)
+    End Sub
+
+    Private Sub testclass1()
+        Throw New NotImplementedException()
+    End Sub
+End Class
