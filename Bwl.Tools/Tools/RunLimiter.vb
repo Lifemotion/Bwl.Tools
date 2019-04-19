0e7766f849378969177a9ab5e8eca8591c123f7a
draf <artem.drobanov@gmail.com>
2019-04-19 10:41:01 +0300

    RunLimiter.

---
+++ Bwl.Tools/Tools/RunLimiter.vb
@@ -0,0 +1,38 @@
+﻿'   Copyright 2019 Artem Drobanov (artem.drobanov@gmail.com)
+
+'   Licensed under the Apache License, Version 2.0 (the "License");
+'   you may Not use this file except In compliance With the License.
+'   You may obtain a copy Of the License at
+
+'     http://www.apache.org/licenses/LICENSE-2.0
+
+'   Unless required by applicable law Or agreed To In writing, software
+'   distributed under the License Is distributed On an "AS IS" BASIS,
+'   WITHOUT WARRANTIES Or CONDITIONS Of ANY KIND, either express Or implied.
+'   See the License For the specific language governing permissions And
+'   limitations under the License.
+
+''' <summary>
+''' Ограничитель частоты вызовов какого-либо кода, например, записи в лог.
+''' </summary>
+Public Class RunLimiter
+    Private _limitSeconds As Double
+    Private _runs As New Dictionary(Of String, DateTime)
+
+    Sub New(Optional limitSeconds As Double = 1)
+        _limitSeconds = limitSeconds
+    End Sub
+
+    Public Function Run(action As Action, Optional actionId As String = "noname") As Boolean
+        Dim res As Boolean = False
+        If Not _runs.ContainsKey(actionId) Then
+            _runs.Add(actionId, DateTime.MinValue)
+        End If
+        If (Now - _runs(actionId)).TotalSeconds >= _limitSeconds Then
+            action()
+            _runs(actionId) = Now
+            res = True
+        End If
+        Return res
+    End Function
+End Class
---
+++ Bwl.Tools/ToolsTests/RunLimiterTest.vb
@@ -0,0 +1,28 @@
+﻿Imports System.IO
+Imports System.Threading
+Imports System.Diagnostics
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class RunLimiterTest
+    <TestMethod()>
+    Public Sub RunLimiterTest()
+        Dim limitSeconds = 0.1
+        Dim runLimiter As New RunLimiter(limitSeconds)
+        Dim activityTimes As New List(Of DateTime)
+        Dim activityFlags As New List(Of Boolean)
+        Dim sw As New Diagnostics.Stopwatch()
+        sw.Start()
+        For i = 1 To 1000
+            Dim res = runLimiter.Run(Sub() activityTimes.Add(Now))
+            activityFlags.Add(res)
+            Thread.Sleep(TimeSpan.FromSeconds(limitSeconds / 10))
+        Next
+        sw.Stop()
+        Assert.AreEqual(activityTimes.Count, activityFlags.Where(Function(item) item).Count) 'Количество положительных флагов должно соотв. отработкам
+        Dim experimentTimeSeconds = sw.Elapsed.TotalSeconds
+        Dim activitiesMustBe = Math.Floor(experimentTimeSeconds / limitSeconds)
+        Dim deltaPerc = Math.Abs(activityTimes.Count - activitiesMustBe) / activitiesMustBe 'Реальная отработка не должна сильно отличаться от расчетной
+        Assert.IsTrue(deltaPerc < 0.1)
+    End Sub
+End Class
