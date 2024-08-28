c2d9af8d2803adc754b871fd70d056c91345b797
draf <artem.drobanov@gmail.com>
2024-08-28 16:04:06 +0300

    Добавлен MicroLogger.

---
+++ Bwl.Tools/Tools/MicroLogger.vb
@@ -0,0 +1,61 @@
+﻿Imports System.IO
+Imports System.Threading
+Imports System.Collections.Concurrent
+
+Public Class MicroLogger
+    Implements IDisposable
+
+    Private _lines As New ConcurrentQueue(Of String)()
+    Private _cts As New CancellationTokenSource()
+    Private _task As Task
+
+    Public Property Path As String
+    Public Property FileName As String
+    Public Property UpdateDelayMs As Integer
+
+    Public Event OnException As EventHandler(Of Exception)
+
+    Public Sub New(path As String, fileName As String, Optional updateDelayMs As Integer = 1000)
+        Me.Path = path
+        Me.FileName = fileName
+        Me.UpdateDelayMs = updateDelayMs
+        _task = Task.Run(Sub() WriteTask(_cts.Token), _cts.Token)
+    End Sub
+
+    Public Sub AddMessage(message As String, Optional messageType As String = "")
+        Dim messageTypeMarker = If(Not String.IsNullOrWhiteSpace(messageType), $" [{messageType}] ", "")
+        _lines.Enqueue($"{DateTime.Now.ToString("<dd.MM.yyyy HH:mm:ss.fff>")}{messageTypeMarker}{message}")
+    End Sub
+
+    Public Overridable Sub Dispose() Implements IDisposable.Dispose
+        Dim task = Interlocked.Exchange(_task, Nothing)
+        If task IsNot Nothing Then
+            _cts.Cancel()
+            task.Wait()
+            _cts.Dispose()
+        End If
+    End Sub
+
+    Private Sub WriteTask(ct As CancellationToken)
+        Do While Not ct.IsCancellationRequested OrElse _lines.Any()
+            Try
+                If _lines.Any() Then
+                    Dim pf = (Me.Path, Me.FileName)
+                    If Not String.IsNullOrWhiteSpace(pf.Path) AndAlso Not Directory.Exists(pf.Path) Then
+                        Directory.CreateDirectory(pf.Path)
+                    End If
+                    Using sw = File.AppendText(IO.Path.Combine(pf.Path, pf.FileName))
+                        Dim line As String = Nothing
+                        While _lines.TryDequeue(line)
+                            sw.WriteLine(line)
+                        End While
+                    End Using
+                End If
+            Catch ex As Exception
+                RaiseEvent OnException(Me, ex)
+            Finally
+                Thread.Sleep(UpdateDelayMs)
+            End Try
+        Loop
+    End Sub
+End Class
---
+++ Bwl.Tools/ToolsTests/MicroLoggerTest.vb
@@ -0,0 +1,64 @@
+﻿Imports System.IO
+Imports System.Threading
+Imports System.Collections.Concurrent
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class MicroLoggerTest
+    <TestMethod()>
+    Public Sub MicroLoggerTest()
+        Dim stringCount = 10000
+        Dim writerCount = 100
+        Dim logPath = String.Empty
+        Dim logFileName = "MicroLoggerTest.log"
+
+        Dim cleaningAction = Sub()
+                                 Try
+                                     File.Delete(Path.Combine(logPath, logFileName))
+                                 Catch
+                                 End Try
+                             End Sub
+
+        For Each updateDelayMs In {10, 100, 1000}
+            'Clean
+            cleaningAction()
+
+            'Write
+            Using logger = New MicroLogger(logPath, logFileName, updateDelayMs)
+                Dim sourceLines As New ConcurrentQueue(Of String)
+                For i = 0 To stringCount - 1
+                    sourceLines.Enqueue(i)
+                Next
+                Dim threads As New Queue(Of Thread)()
+                For i = 0 To writerCount - 1
+                    Dim thr = New Thread(Sub()
+                                             For Each line In sourceLines
+                                                 logger.AddMessage(line, "msg")
+                                             Next
+                                         End Sub) With {.IsBackground = True}
+                    threads.Enqueue(thr)
+                Next
+                threads.AsParallel().ForAll(Sub(thr As Thread) thr.Start())
+                threads.AsParallel().ForAll(Sub(thr As Thread) thr.Join())
+            End Using
+
+            'Read
+            Dim targetLinesDic As New Dictionary(Of String, Integer)
+            For Each line In File.ReadAllLines(Path.Combine(logPath, logFileName))
+                line = line.Split(" ").Last()
+                If Not targetLinesDic.ContainsKey(line) Then
+                    targetLinesDic.Add(line, 0)
+                End If
+                targetLinesDic(line) += 1
+            Next
+            For Each targetLineKVP In targetLinesDic
+                If targetLineKVP.Value <> writerCount Then
+                    Throw New Exception("targetLineKVP.Value <> writerCount")
+                End If
+            Next
+
+            'Clean
+            cleaningAction()
+        Next
+    End Sub
+End Class
